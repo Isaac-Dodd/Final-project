d827e1412e7b3795915f56c382340869f6f95c18
lawrencehy <107524784+lawrencehy@users.noreply.github.com>
2025-12-12 20:14:09 -0800

    Update Resource.h
    -setters and getters
    -moved location field to this class
    -changed pair reservation to pair availability

--- Resource.h
+++ Resource.h
@@ -1,4 +1,28 @@
+#ifndef RESOURCE_H_
+#define RESOURCE_H_
+#include <bits/stdc++.h>
+using namespace std;
 
-class Resource
-{
+class Resource {
+private:
+	int id;
+	string name;
+	string location;
+	pair<double, double> availability;
+	bool checkedOut;
+public:
+	Resource(int id, string n, string l, pair<double, double>a, bool co):
+		id(id), name(n), location(l), availability(a), checkedOut(co){}
+	virtual ~Resource();
+	virtual void getResourceSpecifics() const = 0;
+	int getId(){return id;}
+	string getName(){return name;}
+	bool isCheckedOut(){return checkedOut;}
+	pair<double, double> getAvailability(){return availability;}
+
+	void setId(int id){this->id = id;}
+	void setName(string n){name = n;}
+	void setLocation(string l){location = l;}
+	void setAvailability(pair<double, double> a){availability = a;}
 };
+#endif
