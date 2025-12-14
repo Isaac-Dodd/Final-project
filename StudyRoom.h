f0cff7c3d8514ad2ee2c4c7080fb864b888d7420
lawrencehy <107524784+lawrencehy@users.noreply.github.com>
2025-12-13 16:47:13 -0800

    Update StudyRoom.h

--- StudyRoom.h
+++ StudyRoom.h
@@ -1,1 +1,30 @@
 
+#ifndef STUDYROOM_H_
+#define STUDYROOM_H_
+#include "Resource.h"
+class StudyRoom : public Resource{
+private:
+	int capacity;
+	bool whiteBoard;
+public:
+	StudyRoom(int id,string n, string l, pair<double,double>a, bool co,
+			  int c, bool wb):
+			  Resource(id, n, l, a, co),
+			  capacity(c), whiteBoard(wb) {}
+	~StudyRoom();
+
+	int getCapacity(){return capacity;}
+	bool hasWhiteBoard(){return whiteBoard;}
+
+	void setCapacity(int c){capacity = c;}
+	void setWhiteBoard(bool wb){whiteBoard = wb;}
+
+	void getResourceSpecifics()const
+	{
+		cout << "Capacity: " << capacity << " persons\n"
+			 << "Has white board: ";
+		if(whiteBoard) cout << "Yes\n";
+		else cout << "No\n";
+	}
+};
+#endif
