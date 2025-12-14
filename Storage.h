c2ba2207c57d3d39ef20bae2702bd0a02ff6c919
lawrencehy <107524784+lawrencehy@users.noreply.github.com>
2025-12-13 17:20:43 -0800

    Update Storage.h
    not finished

--- Storage.h
+++ Storage.h
@@ -1,1 +1,42 @@
+#ifndef STORAGE_H_
+#define STORAGE_H_
+#include "Resource.h"
+
+class Storage {
+private:
+	vector<Resource*> storage;
+	unordered_map<int, bool> checkedOutIds;
+
+public:
+	Storage();
+	~Storage();
+	void listResources()
+	{
+		for(auto& i : storage)
+		{
+			i.getResourceSpecifics();
+		}
+	}
+
+	void addResources(Resource* resource)
+	{
+		storage.push_back(resource);
+	}
+
+	void removeResources(Resource* resource)
+	{
+		auto it = find(storage.begin(), storage.end(), resource);
+		storage.erase(it);
+	}
+
+	void listReservations();
+	bool uniqueId(int id)
+	{
+		// if id exists return false
+		if(checkedOutIds.count(id)) return false;
+		return true;
+	}
+};
+
+#endif
 
