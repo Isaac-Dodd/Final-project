36913b5c906b86d67b7bd0cfe7441e9457ad8488
Isaac Dodd <143364036+Isaac-Dodd@users.noreply.github.com>
2025-12-18 18:31:33 -0800

    Update ReservationList.cpp

--- ReservationList.cpp
+++ ReservationList.cpp
@@ -1,39 +1,41 @@
 #include "ReservationList.h"
 
+
 ReservationList::ReservationList()
 {
-	string resourceID;
-	string userName;
-	string reservationStart;
-	string reservationEnd;
-
 	fstream myStream("Reservations.txt", ios::in);
+	string ID;
+	string username;
+	string startDay;
+	string startMonth;
+	string startYear;
+	string endDay;
+	string endMonth;
+	string endYear;
 
 	if (!myStream.is_open())
-		succesfulLoad = false;
-	else
-		succesfulLoad = true;
-
+	{
+		return;
+	}
+	
+	// The txt file is formatted as
+	// ID, username, start day, month, year
+	// then end day, month, year
 	while (!myStream.eof())
 	{
-		// Handling this is gonna come down to what our time is like
-
-		currentReservations.push_back(Reservation(stoi(resourceID), userName, { stod(reservationStart),stod(reservationEnd)}));
+		getline(myStream, ID, ',');		// delimiting based on space, can do comma
+		getline(myStream, username, ',');
+		getline(myStream, startDay, ',');
+		getline(myStream, startMonth, ',');
+		getline(myStream, startYear, ',');
+		getline(myStream, endDay, ',');
+		getline(myStream, endMonth, ',');
+		getline(myStream, endYear, '\n');
+		
+		currentReservations.push_back({ stoi(ID),username,
+			dates{stoi(startDay),stoi(startMonth),stoi(startYear),stoi(endDay),stoi(endMonth),stoi(endYear)} });
+		
+		logByID[stoi(ID)].push_back( Reservation({ stoi(ID),username,
+			dates{stoi(startDay),stoi(startMonth),stoi(startYear),stoi(endDay),stoi(endMonth),stoi(endYear)} }) );
 	}
-}
-
-void ReservationList::createReservation(int resourceID, string userName, pair<double, double> reservation)
-{
-	currentReservations.push_back(Reservation(resourceID, userName, reservation));
-}
-
-// We never discussed how this was gonna get done
-void ReservationList::cancelReservation(int ID)
-{
-
-}
-
-bool ReservationList::isConflict()
-{
-	return true;
 }
