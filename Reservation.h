17547c7b1caa3715978ada4a4c63fe6f1fcb18a0
Isaac Dodd <143364036+Isaac-Dodd@users.noreply.github.com>
2025-12-20 03:13:15 -0800

    Update Reservation.h

--- Reservation.h
+++ Reservation.h
@@ -1,33 +1,9 @@
 #pragma once
-
+#include "Dates.h"
 #include <string>
 using namespace std;
 
-struct Dates
-{
-	int startDay;
-	int startMonth;
-	int startYear;
 
-	int dueDay;
-	int dueMonth;
-	int dueYear;
-
-	bool operator ==(const Dates& rightSide) const
-	{
-		if (startDay != rightSide.startDay || dueDay != rightSide.dueDay)
-			return false;
-
-		else if (startMonth!= rightSide.startMonth || dueMonth != rightSide.dueMonth)
-			return false;
-
-		else if (startYear!= rightSide.startYear || dueYear != rightSide.dueYear)
-			return false;
-
-		else
-			return true;
-	}
-};
 
 class Reservation
 {
