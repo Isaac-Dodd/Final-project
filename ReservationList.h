246abf463227f1857ecbf5b277442c13ba55ff71
Isaac Dodd <143364036+Isaac-Dodd@users.noreply.github.com>
2025-12-19 18:15:56 -0800

    Update ReservationList.h

--- ReservationList.h
+++ ReservationList.h
@@ -4,17 +4,23 @@
 #include <vector>
 #include <fstream>
 #include <unordered_map>
+#include <algorithm>
 
 class ReservationList
 {
 private:
-	// This variable holds all the reservations
-	vector<Reservation> currentReservations;
 	// This variable hold the reservations by ID, useful
 	// for detecting collisions
 	unordered_map<int, vector<Reservation> > logByID;
 public:
 	ReservationList();
-	//bool doesCollide(Reservation);
+	void createReservation(Reservation newReservation);
+	void cancelReservation(Reservation endReservation);
+	
+	bool validTimes(Reservation potentialRes);
+	void displayReservations();
+
+	~ReservationList();
+
 };
 
