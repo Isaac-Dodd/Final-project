f1c72d7beb62984b446293e167b52a414f4eaf89
Isaac Dodd <143364036+Isaac-Dodd@users.noreply.github.com>
2025-12-20 00:13:09 -0800

    Update Resource.cpp

--- Resource.cpp
+++ Resource.cpp
@@ -1,1 +1,37 @@
+#include "Resource.h"
 
+Resource::Resource(int setID, string setName, bool setChecked)
+{
+	id = setID;
+	name = setName;
+	checkedOut = setChecked;
+}
+void Resource::setId(int setID)
+{
+	id = setID;
+}
+void Resource::setName(string setName)
+{
+	name = setName;
+}
+void Resource::setCheckedOut(bool setChecked)
+{
+	checkedOut = setChecked;
+}
+
+// The reservation is tied to the 
+// reservations class, not this one
+// getReservation() const;
+
+bool Resource::isCheckedOut() const
+{
+	return(checkedOut);
+}
+int Resource::getId() const
+{
+	return(id);
+}
+string Resource::getName() const
+{
+	return(name);
+}
