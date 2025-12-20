5388643010291e6d30044473ea7477efccc3c3e8
Isaac Dodd <143364036+Isaac-Dodd@users.noreply.github.com>
2025-12-19 18:14:51 -0800

    Update Reservation.h

--- Reservation.h
+++ Reservation.h
@@ -3,16 +3,30 @@
 #include <string>
 using namespace std;
 
-struct dates
+struct Dates
 {
 	int startDay;
-	int dueDay;
-
 	int startMonth;
-	int dueMonth;
-
 	int startYear;
+
+	int dueDay;
+	int dueMonth;
 	int dueYear;
+
+	bool operator ==(const Dates& rightSide) const
+	{
+		if (startDay != rightSide.startDay || dueDay != rightSide.dueDay)
+			return false;
+
+		else if (startMonth!= rightSide.startMonth || dueMonth != rightSide.dueMonth)
+			return false;
+
+		else if (startYear!= rightSide.startYear || dueYear != rightSide.dueYear)
+			return false;
+
+		else
+			return true;
+	}
 };
 
 class Reservation
@@ -20,15 +34,17 @@ class Reservation
 private:
 	int resourceID;
 	string username; 
-	dates resPeriod;
+	Dates resPeriod;
 public:
-	Reservation(int ID, string username, dates resPeriod);
+	Reservation(int ID, string username, Dates resPeriod);
 	void setID(int);
 	void setUsername(string);
-	void setResPeriod(dates newPeriod);
+	void setResPeriod(Dates newPeriod);
 
 	int getID() const;
 	string getUsername() const;
-	dates getResPeriod() const;
+	Dates getResPeriod() const;
+
+	bool operator==(const Reservation& rightside) const;
 };
 
