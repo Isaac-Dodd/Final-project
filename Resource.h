fce66dc637fb5c3ec2ab42454d668684003c0ff0
Isaac Dodd <143364036+Isaac-Dodd@users.noreply.github.com>
2025-12-20 00:12:53 -0800

    Update Resource.h

--- Resource.h
+++ Resource.h
@@ -1,28 +1,27 @@
-#ifndef RESOURCE_H_
-#define RESOURCE_H_
-#include <bits/stdc++.h>
+#pragma once
+#include <string>
+
 using namespace std;
 
-class Resource {
+class Resource
+{
 private:
 	int id;
 	string name;
-	string location;
-	pair<double, double> availability;
 	bool checkedOut;
 public:
-	Resource(int id, string n, string l, pair<double, double>a, bool co):
-		id(id), name(n), location(l), availability(a), checkedOut(co){}
-	virtual ~Resource();
-	virtual void getResourceSpecifics() const = 0;
-	int getId(){return id;}
-	string getName(){return name;}
-	bool isCheckedOut(){return checkedOut;}
-	pair<double, double> getAvailability(){return availability;}
+	Resource(int setID, string setName, bool setChecked);
+	void setId(int setID);
+	void setName(string setName);
+	void setCheckedOut(bool setChecked);
 
-	void setId(int id){this->id = id;}
-	void setName(string n){name = n;}
-	void setLocation(string l){location = l;}
-	void setAvailability(pair<double, double> a){availability = a;}
+	// The reservation is tied to the 
+	// reservations class, not this one
+	// getReservation() const;
+	int getId() const;
+	string getName() const;
+	bool isCheckedOut() const;
+	virtual void getResourceSpecifics() const = 0;
+	
 };
-#endif
+
