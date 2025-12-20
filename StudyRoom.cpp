ae9f5ef76c5923b0746f09fcc5cce3feabca06ec
Isaac Dodd <143364036+Isaac-Dodd@users.noreply.github.com>
2025-12-20 00:18:03 -0800

    Update StudyRoom.cpp

--- StudyRoom.cpp
+++ StudyRoom.cpp
@@ -1,1 +1,26 @@
+#pragma once
+#include "Resource.h"
+
+/*
+* I would change the available hours to ints for the 
+* opening hour and minute, and the closing hour and
+* minute. Then a string for am and pm
+*/
+
+class StudyRoom : public Resource
+{
+private:
+	int capacity;
+	string availableHours;
+	int capacity;
+private:
+	StudyRoom(int, string, int);
+	string getLocation() const;
+	string getAvailabilityHour() const;
+	int getCapacity() const;
+	void setLocation(string);
+	void setAvailabilityHours(string);
+	int setCapacity(int);
+};
+
 
