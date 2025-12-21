024a65af23f076f1c98a498deda658b32d3e1100
Isaac Dodd <143364036+Isaac-Dodd@users.noreply.github.com>
2025-12-20 21:07:20 -0800

    Update Dates.h

--- Dates.h
+++ Dates.h
@@ -1,25 +1,30 @@
 #pragma once
 struct Dates
 {
+	int startMinute;
+	int startHour;
 	int startDay;
 	int startMonth;
 	int startYear;
 
-	int dueDay;
-	int dueMonth;
-	int dueYear;
+	int endMinute;
+	int endHour;
+	int endDay;
+	int endMonth;
+	int endYear;
 
 	bool operator ==(const Dates& rightSide) const
 	{
-		if (startDay != rightSide.startDay || dueDay != rightSide.dueDay)
+		if (startMinute != rightSide.startMinute || endMinute != rightSide.endMinute)
 			return false;
-
-		else if (startMonth != rightSide.startMonth || dueMonth != rightSide.dueMonth)
+		else if (startHour != rightSide.startHour || endHour != rightSide.endHour)
 			return false;
-
-		else if (startYear != rightSide.startYear || dueYear != rightSide.dueYear)
+		else if (startDay != rightSide.startDay || endDay != rightSide.endDay)
 			return false;
-
+		else if (startMonth != rightSide.startMonth || endMonth != rightSide.endMonth)
+			return false;
+		else if (startYear != rightSide.startYear || endYear != rightSide.endYear)
+			return false;
 		else
 			return true;
 	}
